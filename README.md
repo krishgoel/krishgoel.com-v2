c67c226c70dc93fa1feaa0740aae0708e8f1cc9f
Krish Goel <krishgoel3@gmail.com>
2021-05-13 22:12:41 +0530

    Rethinking typography, added all meta and useless static

--- README.md
+++ README.md
@@ -7,5 +7,5 @@
 [] GitHub repo update
 [] Different scroll speeds for the background objects
 [] Typewriter animation (https://svelte.dev/repl/9dfb73bfa9b34aeea4740fa23f5cde8a?version=3.38.2)
-
+[] Update robots.txt and sitemap
 _Don't read this istfg_
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,27 @@
 <script>
+    let title= "Krish Goel | krishgoel.com";
+    let description = "Hi, I'm Krish, a technophile on a journey to find and fulfill my Ikigau from New Delhi.";
+    let url = "https://krishgoel-v4.vercel.app";
+
     import Typewriter from 'svelte-typewriter/lib/Typewriter.svelte'
 </script>
+
+<svelte:head>
+    <title>{title}</title>
+    <meta name="title" content="{title}" />
+    <meta name="description" content="{description}" />
+    <meta property="og:title" content="{title}" />
+    <meta property="og:url" content="{url}" />
+    <meta property="og:description" content="{description}" />
+    <meta name="twitter:title" content="{title}" />
+    <meta name="twitter:url" content="{url}" />
+    <meta name="twitter:description" content="{description}" />
+</svelte:head>
 
 <style>
     .banner {
         width: 100%;
-        background-image: linear-gradient(90deg, var(--navy-background), #00000000 70%), linear-gradient(0deg, var(--navy-background) 5%, #00000000 20%), url(/images/banner.jpeg);
+        background-image: linear-gradient(90deg, var(--navy-background), #00000000 70%), linear-gradient(0deg, var(--navy-background) 7%, #00000000 20%), url(/images/banner.jpeg);
         background-position: 60% 20%;
         background-repeat: no-repeat;
         background-size: cover;
@@ -15,46 +31,26 @@
         align-items: center;
     }
 
-    .banner-text-flipping h2 {
+    /* .banner-text-flipping h2 {
         float: left;
-        overflow-wrap: break-word;
-    }
+    } */
 </style>
 
 <section>
-
     <section class="banner">
         <div class="width-restriction">
-            <h2>Hi, I'm</h2>
-
-            <h1 style="font-size: calc(3rem + 2vw)">Krish</h1>
-
-            <p class="mono">(short for Krishnamangalrathnamiyer Amanunununununununu)</p>
-            <!-- <Typewriter cascade>
-            </Typewriter> -->
-
-            <br/>
-
-            <div class="banner-text-flipping">
+            <!-- <div class="banner-text-flipping">
                 <h2>and manz&nbsp;</h2>
                 <Typewriter loop interval={30}>
-                    <!-- <div></div> -->
                     <h2 class="glow-green">a fan of ya mum jokes</h2>
                     <h2 class="glow-pink">against the degenerates who price Flaming Hot Cheetos in India</h2>
                     <h2 class="glow-blue">a web developer</h2>
                     <h2 class="glow-yellow">a financial literacy enthusiast</h2>
                 </Typewriter>
-            </div>
+            </div> -->
 
-            <br/><br/><br/>
+            <br/><br/><br/><br/>
 
         </div>
     </section>
-    
-    <section>
-        <div class="width-restriction">
-            <h3>Test</h3>
-        </div>
-    </section>
-
 </section>
