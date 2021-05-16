4377cebb499a21de4ac6b54a93651e9c23c9343d
Krish Goel <krishgoel3@gmail.com>
2021-05-16 21:40:08 +0530

    Still experimenting with the aesthetic

--- README.md
+++ README.md
@@ -8,4 +8,7 @@
 [] Different scroll speeds for the background objects
 [] Typewriter animation (https://svelte.dev/repl/9dfb73bfa9b34aeea4740fa23f5cde8a?version=3.38.2)
 [] Update robots.txt and sitemap
+
+Before pushing to production, ensure that the meta URLs are changed to krishgoel.com and not krishgoel-v4.vercel.app 
+
 _Don't read this istfg_
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,36 +21,53 @@
 <style>
     .banner {
         width: 100%;
-        background-image: linear-gradient(90deg, var(--navy-background), #00000000 70%), linear-gradient(0deg, var(--navy-background) 7%, #00000000 20%), url(/images/banner.jpeg);
+        background-image: linear-gradient(90deg, var(--dark-background), #00000000 70%), linear-gradient(0deg, var(--dark-background) 5%, #00000000 20%), url(/images/banner.jpeg);
         background-position: 60% 20%;
         background-repeat: no-repeat;
         background-size: cover;
         height: 90vh;
-        /* width: 100vw; */
         display: flex;
         align-items: center;
     }
 
-    /* .banner-text-flipping h2 {
+    .banner-text-flipping {
+        width: 100%;
+        display: block;
+        height: 100px
+    }
+    .banner-text-flipping h2 {
         float: left;
-    } */
+        overflow-wrap: break-word;
+    }
 </style>
 
 <section>
+
     <section class="banner">
         <div class="width-restriction">
-            <!-- <div class="banner-text-flipping">
-                <h2>and manz&nbsp;</h2>
-                <Typewriter loop interval={30}>
-                    <h2 class="glow-green">a fan of ya mum jokes</h2>
-                    <h2 class="glow-pink">against the degenerates who price Flaming Hot Cheetos in India</h2>
-                    <h2 class="glow-blue">a web developer</h2>
-                    <h2 class="glow-yellow">a financial literacy enthusiast</h2>
-                </Typewriter>
-            </div> -->
+            <div class="row">
+                <div class="col-2">
 
-            <br/><br/><br/><br/>
+                    <h1>Hi, I am Krish</h1>
+                    <p class="mono">(short for Krishnamangalrathnam nunununun)</p>
+        
+                    <br/>
+        
+                    <div class="banner-text-flipping">
+                        <h2>and manz&nbsp;</h2>
+                        <Typewriter loop interval={30} scramble>
+                            <h2 class="glow-blue">a web developer</h2>
+                            <h2 class="glow-green">a fan of ya mum jokes</h2>
+                            <h2 class="glow-pink">against the degenerates who price Flaming Hot Cheetos in India</h2>
+                            <h2 class="glow-yellow">a financial literacy enthusiast</h2>
+                        </Typewriter>
+                    </div>
+
+                </div>
+            </div>
 
         </div>
     </section>
+    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
 </section>
