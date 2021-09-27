be37db0c6133db7bcfd1564a7d46daceacc3108f
Krish Goel <krishgoel3@gmail.com>
2021-09-27 12:11:27 +0530

    Minor changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -69,8 +69,6 @@
     main {
         background-size: 120px 120px;
         background-image:
-            /* linear-gradient(to right, rgb(60,60,60) 1px, transparent 1px),
-            linear-gradient(to bottom, rgb(60,60,60) 1px, transparent 1px); */
             linear-gradient(to right, rgba(60, 60, 60, 0.7) 1px, transparent 1px),
             linear-gradient(to bottom, rgba(60, 60, 60, 0.7) 1px, transparent 1px);
     }
@@ -112,25 +110,8 @@
 
 <main>
     <Banner/>
+
     <Nav appearance={"light"}/>
-    <!-- <section class="intro">
-        <div class="width-restriction">
-            <div class="row">
-                <div class="col-2">
-                    <img src="/images/profile.jpeg"/>
-                </div>
-                <div class="col-2">
-                    <div class="width-restriction">
-                        <h2>KrishSkywalker aka <span class="swag">kringefest</span></h2>
-                        <p>I am a technophile and a full-time "flex machine" from New Delhi. I am currently completing my high schooling from <a href="http://sanskritischool.edu.in" target="_blank">Sanskriti School</a> and will be joining <a href="#" target="_blank">Some University</a> for pursuing every Indian dads&apos; dream for their son - <s>MBA</s>  Engineering Major (+ Elective) this fall.</p>
-                        <p>While web-development and programming is my m&eacute;tier, I&rsquo;m currently in the search for my Ikigai.</p>
-                        <p>I occasionally venture into introducing the beautiful intricacies of programming to the younger students during my summer breaks and looking at the bigger picture, I aspire to develop &quot;smarter energy sources&quot;.</p>
-                        <p>Welcome to <s>my website/secret base</s> Jaku! &nbsp;(btw irrespective of the <em>kringe,</em> people <strong>LIKE</strong> to call me <span class="mono">KrishSkywalker</span> &macr;\(ツ)/&macr;. <span class="swag">Yes</span> 😤)</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> -->
 
     <section class="projects ">
         <div class="width-restriction">
@@ -197,7 +178,7 @@
                         <p><a href="/garden">See all ></a></p>
                     </div>
                     <Space height={"5px"}/>
-                    <p>This is a digital garden (more on that <a href="https://cagrimmett.com/notes/2020/11/08/what-are-digital-gardens/" target="_blank">here</a>). It's essentially me publishing my notes and updating them periodically with new ideas and insights I gain on the topic, expectedly about technology, things I am using, or about the time I went backpacking to the foothills of mount Tibidabo ;)</p>                        
+                    <p>This is a digital garden (more on that <a href="https://cagrimmett.com/notes/2020/11/08/what-are-digital-gardens/" target="_blank">here</a>). It's essentially me publishing my notes and updating them periodically with new ideas and insights I gain on the topic, expectedly about technology, things I am using, or about the time I went backpacking to the foothills of mount Tibidabo ;) <i>This will take some more time for me to fully organize</i></p>                        
                 </div>
             </div>
             <Space height={"25px"}/>
