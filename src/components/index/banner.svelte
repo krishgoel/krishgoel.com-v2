aca706a7c106df94ab24fd0296e841e2b0251a03
Krish Goel <krishgoel3@gmail.com>
2021-10-05 19:36:11 +0530

    Grammatical fix

--- src/components/index/banner.svelte
+++ src/components/index/banner.svelte
@@ -34,7 +34,7 @@
                 <Space height={"12px"}/>
 
                 <p>I am a technophile and a full-time "flex machine" from New Delhi. I recently completed my high school from <a href="http://sanskritischool.edu.in" target="_blank" aria-label="Sanskriti School">Sanskriti School</a> and will be joining <a href="https://manipal.edu/mit.html" target="_blank" aria-label="MIT, Manipal">Manipal Institute of Technology</a> for pursuing every Indian dads&apos; dream for their son - <s>MBA</s>  BTech (in Mechatronics) this fall.</p>
-                <p>Currently, my interest lies at the intersection of CS, cyber-physical systems, and behavioral psychology along with startups and finances. During my summer breaks, I occasionally venture into introducing the beautiful intricacies of programming to younger students. Looking at the bigger picture, I aim to eliminate the redundancy of human-computer interactions and develop &quot;smarter&quot; energy sources.</p>
+                <p>Currently, my interest lies at the intersection of CS, cyber-physical systems, and behavioral psychology alongside startups and finances. During my summer breaks, I occasionally venture into introducing the beautiful intricacies of programming to younger students. Looking at the bigger picture, I aim to eliminate the redundancy of human-computer interactions and develop &quot;smarter&quot; energy sources.</p>
                 <!-- <p>Welcome to <s>my website</s> the bent side of things (btw irrespective of the <span class="swag">kringe,</span> people <strong>LIKE</strong> to call me <span class="mono">KrishSkywalker</span> &macr;\(ツ)/&macr;. Yes)</p> -->
 
                 <!-- <Space height={"15px"}/> -->
