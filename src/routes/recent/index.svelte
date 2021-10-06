2b3f9f4233d90b4b83b94c7ca2c0b7068c5ecc6c
Krish Goel <krishgoel3@gmail.com>
2021-10-06 20:53:08 +0530

    Disabled workflows, fixed /recent and made toptracks into a workflow

--- src/routes/recent/index.svelte
+++ src/routes/recent/index.svelte
@@ -1,19 +1,25 @@
+<script context="module">
+    export async function preload() {
+        let response = await this.fetch('/data/toptracks.json');
+        let responseJson = await response.json();
+        responseJson = responseJson.toptracks.track
+        return {
+            toptracks: responseJson
+        }
+    }
+</script>
+
 <script>
 	import fetch from 'cross-fetch'
 
     import Space from '../../components/space.svelte'
 
-    const topTracks = (async () => {
-        let response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&period=7day&user=KrishSkywalker&api_key=faa73d570313176a337af4dea9827765&format=json&limit=5`)
-        response = await response.json()
-        return response.toptracks.track
+    const livelistening = (async () => {
+        var response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=KrishSkywalker&api_key=faa73d570313176a337af4dea9827765&format=json&limit=1`)
+        return await response.json()
     })()
 
-    const readlist = (async () => {
-        let response = await fetch(`https://potion-api.vercel.app/table?id=ed5a59a2d5924e6f81044a1ac9247f97`)
-        response = await response.json()
-        return response
-    })()
+    export let toptracks
 
     let title= "Recent Activity | krishgoel.com";
     let description = "Hi, this page reflects the recent activities of Krish, a technophile and maker from New Delhi.";
@@ -53,61 +59,42 @@
 </style>
 
 <section class="recent slim-container">
-    <h1>Recent Activity</h1>
-    <Space height={"25px"}/>
-    <div class="row">
-        <div class="col-2">
-            <div class="inline">
-                <h2>Top Tracks</h2>
-                <p><a href="https://last.fm/user/KrishSkywalker" target="_blank" aria-label="My Last.fm">Last.fm ></a></p>
-            </div>
-            <Space height={"10px"}/>
-            <div class="card">
-                <div class="width-restriction">
-                    {#await topTracks}
-                    <!--  -->
-                    {:then data}
-                        {#each data as track, i}
-                            {#if i != 4}
-                                <h4>{track.name}</h4>
-                                <p>by {track.artist.name}</p>
-                                <hr/>
-                            {:else}
-                                <h4>{track.name}</h4>
-                                <p style="margin-bottom: 0">by {track.artist.name}</p>
-                            {/if}
-                        {/each}
-                    {/await}
-                </div>
-            </div>
+    <div class="inline">
+        <h1>Top Tracks</h1>
+        <p style="margin-left: 5px">From this week</p>
+        <p><a href="https://last.fm/user/KrishSkywalker" target="_blank" aria-label="My Last.fm">Last.fm ></a></p>
+    </div>
+    <Space height={"20px"}/> 
+    {#await livelistening}
+        <!--  -->
+    {:then data}
+    <div class="card">
+        <div class="width-restriction">
+            {#if data.recenttracks.track[0].hasOwnProperty("@attr")}
+                <p style="margin-bottom: 10px">Currently listening to</p>
+            {:else}
+                <p style="margin-bottom: 10px">Last listened to</p>
+            {/if}
+            <h3 style="margin-bottom: 10px">{data.recenttracks.track[0].name}</h3>
+            <p style="margin-bottom: 0px">by <strong>{data.recenttracks.track[0].artist["#text"]}</strong></p>
         </div>
-        <div class="col-2">
-            <div class="inline">
-                <h2>Reading</h2>
-                <p><a href="https://krishgoel.notion.site/ed5a59a2d5924e6f81044a1ac9247f97?v=6f977945ff954f4e91c1be3a4ea83295" target="_blank" aria-label="My Read List">My Read List ></a></p>
-            </div>
-            <Space height={"10px"}/>
-            <div class="card">
-                <div class="width-restriction">
-                    <p>Currently reading</p>
-                    <Space height={"10px"}/>
-                    {#await readlist}
-                        <!--  -->
-                    {:then data}
-                        {#each data as book, i}
-                            {#if book.fields.Status == "Currently reading"}
-                                <h4>{book.fields.Book}</h4>
-                                <Space height={"10px"}/>
-                                <p>by <strong>{book.fields.Author}</strong></p>
-                            {/if}
-                        {/each}
-                    {/await}
-                </div>
-            </div>
-            <div class="inline">
-                <h2>Learning</h2>
-                <p><a href="https://krishgoel.notion.site/Learn-List-39a102bb06044a3cbd0830cd6555b6ca" target="_blank" aria-label="My Learn List">My Learn List ></a></p>
-            </div>
+    </div>
+    {:catch error}
+            <!--  -->
+    {/await}
+    <Space height={"10px"}/>
+    <div class="card">
+        <div class="width-restriction">
+            {#each toptracks as track, i}
+                {#if i != 4}
+                    <h4>{track.name}</h4>
+                    <p>by {track.artist.name}</p>
+                    <hr/>
+                {:else}
+                    <h4>{track.name}</h4>
+                    <p style="margin-bottom: 0">by {track.artist.name}</p>
+                {/if}
+            {/each}
         </div>
     </div>
 </section>
