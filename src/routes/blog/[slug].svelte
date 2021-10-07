7dff8a7ceb731e7cec8da239d3fcbac2427607ba
Krish Goel <krishgoel3@gmail.com>
2021-10-07 17:41:00 +0530

    Trying to structure the Netlify CMS for the blog

---
+++ src/routes/blog/[slug].svelte
@@ -0,0 +1,39 @@
+<script context="module">
+	export async function preload({ params, query }) {
+		const res = await this.fetch(`blog/${params.slug}.json`);
+		const data = await res.json();
+		if (res.status === 200) {
+			return { post: data };
+		} else {
+			this.error(res.status, data.message);
+		}
+	}
+</script>
+
+<script>
+    import Space from '../../components/space.svelte'
+
+	export let post;
+
+    let title= post.title + " | krishgoel.com";
+    let description = "Hi, I'm Krish, a technophile and maker from New Delhi and this is my blog.";
+    let url = "https://krishgoel.com/blog/" + post.title;
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+    <meta name="title" content="{title}" />
+    <meta name="description" content="{description}" />
+    <meta property="og:title" content="{title}" />
+    <meta property="og:url" content="{url}" />
+    <meta property="og:description" content="{description}" />
+    <meta name="twitter:title" content="{title}" />
+    <meta name="twitter:url" content="{url}" />
+    <meta name="twitter:description" content="{description}" />
+</svelte:head>
+
+<section class="slim-container">
+    <p>{post.date} · {post.readtime}</p>
+    <Space/>
+	{@html post.html}
+</section>
--- static/posts/this-is-a-test-ting.md
+++ static/posts/this-is-a-test-ting.md
@@ -2,4 +2,4 @@
 slug: test
 title: This is a test ting
 ---
-```Woo Woo Woo```
+# Test
