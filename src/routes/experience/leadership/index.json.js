9ef9cd63c11a608ccf7be55baf32e3f67426f302
Krish Goel <krishgoel3@gmail.com>
2021-10-11 00:31:18 +0530

    Added sorting to leadership.json

--- src/routes/experience/leadership/index.json.js
+++ src/routes/experience/leadership/index.json.js
@@ -20,6 +20,16 @@ export function get(_, res) {
   res.writeHead(200, {
     "Content-Type": "application/json",
   });
-  const leaderships = getAllLeadership();
-  res.end(JSON.stringify(leaderships));
+  let leaderships = getAllLeadership();
+  
+  // For sorting by date
+  // Splits the date into MM and YYYY and compares the year value with preference
+  leaderships = leaderships.sort(function (a, b) {
+    return parseInt(a.datestamp.substring(3,7)) - parseInt(b.datestamp.substring(3,7)) || parseInt(a.datestamp.substring(0,2)) - parseInt(b.datestamp.substring(0,2))
+  });
+  // Reverse to arrange newest - oldest
+  leaderships = leaderships.reverse()
+
+  leaderships = JSON.stringify(leaderships);
+  res.end(leaderships);
 }
