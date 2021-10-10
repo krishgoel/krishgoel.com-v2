d90342fd57004e6bdc665586334df5a470f0868e
Krish Goel <krishgoel3@gmail.com>
2021-10-11 00:27:58 +0530

    Added the datestamp field to all CMS collections and added sorting to dubs.json

--- src/routes/experience/dubs/index.json.js
+++ src/routes/experience/dubs/index.json.js
@@ -20,6 +20,16 @@ export function get(_, res) {
   res.writeHead(200, {
     "Content-Type": "application/json",
   });
-  const dubs = getAllDubs();
-  res.end(JSON.stringify(dubs));
+  let dubs = getAllDubs();
+  
+  // For sorting by date
+  // Splits the date into MM and YYYY and compares the year value with preference
+  dubs = dubs.sort(function (a, b) {
+    return parseInt(a.datestamp.substring(3,7)) - parseInt(b.datestamp.substring(3,7)) || parseInt(a.datestamp.substring(0,2)) - parseInt(b.datestamp.substring(0,2))
+  });
+  // Reverse to arrange newest - oldest
+  dubs = dubs.reverse()
+
+  dubs = JSON.stringify(dubs);
+  res.end(dubs);
 }
