370e8da3b51e9dcf030cd86f43b7ab4020646fa2
Krish Goel <krishgoel3@gmail.com>
2021-10-11 00:31:36 +0530

    Added sorting to socialservice.json

--- src/routes/experience/socialservice/index.json.js
+++ src/routes/experience/socialservice/index.json.js
@@ -20,6 +20,16 @@ export function get(_, res) {
   res.writeHead(200, {
     "Content-Type": "application/json",
   });
-  const socialservices = getAllSocialService();
-  res.end(JSON.stringify(socialservices));
+  let socialservices = getAllSocialService();
+  
+  // For sorting by date
+  // Splits the date into MM and YYYY and compares the year value with preference
+  socialservices = socialservices.sort(function (a, b) {
+    return parseInt(a.datestamp.substring(3,7)) - parseInt(b.datestamp.substring(3,7)) || parseInt(a.datestamp.substring(0,2)) - parseInt(b.datestamp.substring(0,2))
+  });
+  // Reverse to arrange newest - oldest
+  socialservices = socialservices.reverse()
+
+  socialservices = JSON.stringify(socialservices);
+  res.end(socialservices);
 }
