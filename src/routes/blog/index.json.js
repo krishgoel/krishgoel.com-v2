0037aa60e60cc2b9a9107cd447f71cc1e0f8f9c4
Krish Goel <krishgoel3@gmail.com>
2021-10-11 00:22:19 +0530

    Added date sorting to blog

--- src/routes/blog/index.json.js
+++ src/routes/blog/index.json.js
@@ -20,6 +20,16 @@ export function get(_, res) {
   res.writeHead(200, {
     "Content-Type": "application/json",
   });
-  const posts = getAllPosts();
-  res.end(JSON.stringify(posts));
+  let posts = getAllPosts();
+  
+  // For sorting by date
+  // Splits the date into MM and YYYY and compares the year value with preference
+  posts = posts.sort(function (a, b) {
+    return parseInt(a.datestamp.substring(3,7)) - parseInt(b.datestamp.substring(3,7)) || parseInt(a.datestamp.substring(0,2)) - parseInt(b.datestamp.substring(0,2))
+  });
+  // Reverse to arrange newest - oldest
+  posts = posts.reverse()
+
+  posts = JSON.stringify(posts);
+  res.end(posts);
 }
