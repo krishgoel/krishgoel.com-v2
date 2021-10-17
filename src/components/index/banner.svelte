f223cfad36cb1a253c6a807a441c1466df52f0a0
Krish Goel <krishgoel3@gmail.com>
2021-10-18 03:48:53 +0530

    Added the banner

--- src/components/index/banner.svelte
+++ src/components/index/banner.svelte
@@ -1,9 +1,9 @@
 <style>
     .banner {
         width: 100%;
-        background-image: linear-gradient(90deg, var(--dark-background), #00000000 70%), url(/images/uploads/banner.jpg);
+        background-image: linear-gradient(90deg, var(--dark-background), #00000000 70%), url(/images/uploads/banner.PNG);
         /* background-image: linear-gradient(90deg, var(--dark-background), #00000000 70%); */
-        background-position: 50% 20%;
+        background-position: 75% 90%;
         background-repeat: no-repeat;
         background-size: cover;
         background-color: var(--dark-background);
@@ -12,6 +12,11 @@
         display: flex;
         align-items: center;
     }
+    .banner mark {
+        color: var(--light-text);
+        background-color: #16161666
+    }
+
     .banner p {
         line-height: 155%;
     }
@@ -32,12 +37,18 @@
                 <h1>I'm Krish</h1>
 
                 <Space height={"12px"}/>
-
-                <p>I am a technophile and a full-time "flex machine" from New Delhi. I recently completed my high school from <a href="http://sanskritischool.edu.in" target="_blank" aria-label="Sanskriti School">Sanskriti School, New Delhi</a> and will be joining <a href="https://manipal.edu/mit.html" target="_blank" aria-label="MIT, Manipal">Manipal Institute of Technology</a> for pursuing every Indian dads&apos; dream for their son - <s>MBA</s>  BTech (in Mechatronics) this fall.</p>
-                <p>Currently, my interest lies at the intersection of CS, cyber-physical systems, and behavioral psychology alongside startups and finances. During my summer breaks, I occasionally venture into introducing the beautiful intricacies of programming to younger students. Looking at the bigger picture, I aim to eliminate the redundancy of human-computer interactions and develop &quot;smarter&quot; energy sources.</p>
+                
+                <p>
+                    <mark>I am a technophile and a full-time "flex machine" from New Delhi. I recently completed my high school from <a href="http://sanskritischool.edu.in" target="_blank" aria-label="Sanskriti School">Sanskriti School, New Delhi</a> and will be joining <a href="https://manipal.edu/mit.html" target="_blank" aria-label="MIT, Manipal">Manipal Institute of Technology</a> for pursuing every Indian dads&apos; dream for their son - <s>MBA</s>  BTech (in Mechatronics) this fall.</mark>
+                </p>
+                <p>
+                    <mark>Currently, my interest lies at the intersection of CS, cyber-physical systems, and behavioral psychology alongside startups and finances. During my summer breaks, I occasionally venture into introducing the beautiful intricacies of programming to younger students. Looking at the bigger picture, I aim to eliminate the redundancy of human-computer interactions and develop &quot;smarter&quot; energy sources.</mark>
+                </p>
 
                 <Typewriter>
-                    <p class="swag">and I am against the degenerates who price Flaming Hot Cheetos in India :)</p>
+                    <p class="swag">
+                        <mark>and I am against the degenerates who price Flaming Hot Cheetos in India :)</mark>
+                    </p>
                 </Typewriter>
             </div>
         </div>
