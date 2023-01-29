42fe63a7fa89f497e7c4775c161e7f86d9fd5951
Krish Goel <krishgoel3@gmail.com>
2023-01-29 15:12:08 +0530

    Added /hiring

--- src/routes/hiring.svelte
+++ src/routes/hiring.svelte
@@ -1,6 +1,22 @@
 <script>
-    import Space from '../components/space.svelte'
+	let title= "Hiring | krishgoel.com";
+    let description = "Hi, I'm Krish, a technophile and maker from New Delhi and we're hiring.";
+    let url = "https://krishgoel.com/colophon";
+	
+	import Space from '../components/space.svelte'
 </script>
+
+<svelte:head>
+    <title>{title}</title>
+    <meta name="title" content="{title}" />
+    <meta name="description" content="{description}" />
+    <meta property="og:title" content="{title}" />
+    <meta property="og:url" content="{url}" />
+    <meta property="og:description" content="{description}" />
+    <meta name="twitter:title" content="{title}" />
+    <meta name="twitter:url" content="{url}" />
+    <meta name="twitter:description" content="{description}" />
+</svelte:head>
 
 <section>
 	<h1>We're Hiring</h1>
